f116558aa17407d76817902867b83c245fc38f82
AngelRivera <20233l01142@utcv.edu.mx>
2025-02-11 20:09:17 -0600

    feat: agregar servicio para auth
    se agrego el servicio para login
    
    se agrego servicio para register
    
    se agrego servicio para logout
    
    se agrego funcion para generar tokens

--- src/app/authentication/services/auth.service.ts
+++ src/app/authentication/services/auth.service.ts
@@ -6,4 +6,39 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  register(user: { name: string; email: string; password: string; }) {
+    // Guardamos los datos en localStorage simulando un registro    
+    localStorage.setItem('userEmail', user.email);
+    localStorage.setItem('userPassword', user.password);
+    localStorage.setItem('Username', user.name);
+  }
+
+  login(email: string, password: string, name: string): boolean {
+    const storedEmail = localStorage.getItem('userEmail');
+    const storedPassword = localStorage.getItem('userPassword');   
+    console.log(localStorage.getItem('userName'));   
+    console.log(localStorage.getItem('userPassword'));   
+   
+    if (email === storedEmail && password === storedPassword ) {
+      const token = this.generateToken(name);  // Generar el token
+      this.storeToken(token);  // Almacenar el token en localStorage        
+  
+      return true; // Inicio de sesión exitoso
+    }
+    return false; // Credenciales incorrectas
+  }
+
+  generateToken(name: string): string {
+    return btoa(name + ':' + new Date().getTime()); // Genera un token básico con el nombre y el timestamp
+  }
+
+  storeToken(token: string) {
+    localStorage.setItem('userToken', token);
+  }
+
+  logout() {
+    localStorage.removeItem('userToken');
+  }
+
 }
